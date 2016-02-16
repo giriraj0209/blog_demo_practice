e39d0653b5d8e468bfc78208372e101341379efc
giriraj0209 <giriraj.kashyapa@gmail.com>
2016-02-15 21:24:03 -0600

    Added turbolink gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require turbolinks
 //= require jquery_ujs
+//= require materialize-sprockets
 //= require turbolinks
 //= require_tree .
